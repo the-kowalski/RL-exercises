00aede6f71d12a90e853a33405cfdc2fb42f1fec
Thorben Kurowski <thorben.kurowski@googlemail.com>
2026-05-19 09:29:17 +0200

    Week 4

--- rl_exercises/week_4/buffers.py
+++ rl_exercises/week_4/buffers.py
@@ -59,10 +59,20 @@ class ReplayBuffer(AbstractBuffer):
         """
         if len(self.states) >= self.capacity:
             # TODO: pop the oldest element off each list (states, actions, …, infos)
-            # pop oldest
-            return
+            self.states.pop(0)
+            self.actions.pop(0)
+            self.rewards.pop(0)
+            self.next_states.pop(0)
+            self.dones.pop(0)
+            self.infos.pop(0)
 
         # TODO: append state, action, reward, next_state, done, info to their respective lists
+        self.states.append(state)
+        self.actions.append(int(action))
+        self.rewards.append(reward)
+        self.next_states.append(next_state)
+        self.dones.append(done)
+        self.infos.append(info)
 
     def sample(
         self, batch_size: int = 32
@@ -80,7 +90,7 @@ class ReplayBuffer(AbstractBuffer):
         List of transitions as (state, action, reward, next_state, done, info).
         """
         # TODO: randomly choose `batch_size` unique indices from [0, len(self.states))
-        idxs = ...
+        idxs = np.random.choice(len(self.states), batch_size, replace=False)
         return [
             (
                 self.states[i],
